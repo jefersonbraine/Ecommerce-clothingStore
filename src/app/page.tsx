96e11574a9a6a115a3cffb34d85640ff78db2d36
Jeferson <jefeleal363@gmail.com>
2025-08-04 21:28:56 -0300

    chore: add eslint, prettier and shadcn setup

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div>
+      <Button className="my-1 bg-red-500 px-5">Bootcamp</Button>
+    </div>
+  );
+};
+
+export default Home;
