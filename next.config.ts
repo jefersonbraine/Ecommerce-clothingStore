e1564ffee5292cad30718bb77fde0eabc3d64c08
Jeferson <jefeleal363@gmail.com>
2025-08-09 21:10:32 -0300

    feat: add proudct list and product item components

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d4lgxe9bm8juw.cloudfront.net",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
